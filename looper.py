ea76559fc6febaefa117ac4dd058e7d88fa446e2
Justin <jj4123@nyu.edu>
2024-10-14 22:15:58 -0400

    use .ogg files + clean up

--- looper.py
+++ looper.py
@@ -13,15 +13,15 @@ mixer.pre_init(44100, -16, 2, 2048)
 mixer.init()
 
 # set audio channels
-acapella = mixer.Channel(0)
-melody = mixer.Channel(1)
-beat = mixer.Channel(2)
+# acapella = mixer.Channel(0)
+# melody = mixer.Channel(1)
+# beat = mixer.Channel(2)
 
 # set audio files
 in_style = {
-    'acapella': mixer.Sound('audio/masego-in-style/in_style-acapella.wav'),
-    'melody': mixer.Sound('audio/masego-in-style/in_style-melody.wav'),
-    'beat': mixer.Sound('audio/masego-in-style/in_style-beat.wav')
+    'acapella': mixer.Sound('audio/masego-in-style/in_style-acapella.ogg'),
+    'melody': mixer.Sound('audio/masego-in-style/in_style-melody.ogg'),
+    'beat': mixer.Sound('audio/masego-in-style/in_style-beat.ogg')
     }
 
 in_style['acapella'].set_volume(0)
@@ -30,29 +30,26 @@ in_style['beat'].set_volume(0)
 
 try:
     while True: # program loop
-        # acapella.play(in_style['acapella'], -1)
         in_style['acapella'].play()
         in_style['melody'].play()
         in_style['beat'].play()
-        # melody.play(in_style['melody'])
-        # beat.play(in_style['beat'])
         if GPIO.input(11):
             if in_style['acapella'].get_volume() > 0:
                 in_style['acapella'].set_volume(0)
             else: 
-                in_style['acapella'].set_volume(0.3)
+                in_style['acapella'].set_volume(0.5)
             sleep(0.2)
         elif GPIO.input(13):
             if in_style['melody'].get_volume() > 0:
                 in_style['melody'].set_volume(0)
             else: 
-                in_style['melody'].set_volume(0.3)
+                in_style['melody'].set_volume(0.5)
             sleep(0.2)
         elif GPIO.input(15):
             if in_style['beat'].get_volume() > 0:
                 in_style['beat'].set_volume(0)
             else: 
-                in_style['beat'].set_volume(0.3)
+                in_style['beat'].set_volume(0.5)
             sleep(0.2)
         
 finally:
