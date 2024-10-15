e75b342704c20f4d278e3d7f09ef5ce42de325e8
Justin <jj4123@nyu.edu>
2024-10-14 20:09:52 -0400

    it works!!

--- audio_test.py
+++ audio_test.py
@@ -1,5 +1,15 @@
-import pygame
-pygame.init()
+# Thank you stack overflow: https://stackoverflow.com/questions/43758408/how-to-play-a-sound-using-pygame-in-all-operating-system
 
-masego = pygame.mixer.Sound('audio/msgo1.wav')
-masego.play()
+from pygame import mixer
+mixer.pre_init(44100, -16, 2, 2048)
+mixer.init()
+
+masego = mixer.Sound('audio/masego-in-style.wav')
+
+def play_masego():
+    masego.play()
+    print('Playing Masego!!')
+
+while True:
+    inpt = input('Press enter to play:')
+    play_masego()
