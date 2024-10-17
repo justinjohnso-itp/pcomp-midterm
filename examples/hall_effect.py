4f484274c59436c1dc1ab0581e39d51a3ca1d127
Justin <jj4123@nyu.edu>
2024-10-17 17:25:26 -0400

    add hall effect sensor test

---
+++ examples/hall_effect.py
@@ -0,0 +1,10 @@
+# import and set up GPIO and time packages
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN) # pin 11 input
+GPIO.setup(13, GPIO.IN) # pin 13 input
+GPIO.setup(15, GPIO.IN) # pin 15 input
+
+print(GPIO.input(11))
