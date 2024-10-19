47e98aabbe44ae9bfa8af6cb3547059fddb800d1
Justin <jj4123@nyu.edu>
2024-10-18 21:06:13 -0400

    neopixel testing

--- examples/neopixel_test.py
+++ examples/neopixel_test.py
@@ -1,8 +1,30 @@
+# init libraries
 import board
 import neopixel
 
-DATA_PIN = board.D10
-pixel = neopixel.NeoPixel(DATA_PIN, ...)
-# pixels = neopixel.NeoPixel(board.D10, 7)
-# pixels[1] = (255, 0, 0)
-# print(pixels)
+from adafruit_led_animation.helper import PixelSubset
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.animation.pulse import Pulse
+from adafruit_led_animation.animation.sparkle import Sparkle
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.group import AnimationGroup
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation.color import *
+
+pixels = neopixel.NeoPixel(board.D10, 144 + 7, brightness=0.1, auto_write=True)
+
+led_strip = PixelSubset(pixels, 8, 151)
+star = PixelSubset(pixels, 0, 7)
+
+animations = AnimationSequence(
+    AnimationGroup(
+        Pulse(star, speed=0.05, color=PURPLE, period=2),
+        Comet(led_strip, speed=0.01, color=AMBER, tail_length=20),
+        sync=True,
+    )
+)
+
+while True:
+    animations.animate()
