ca0e6551d6ab43b933ca2bfdca746b382e07e50e
Justin <jj4123@nyu.edu>
2024-10-21 19:40:11 -0400

    set up individual neopixels

--- examples/neopixel_test.py
+++ examples/neopixel_test.py
@@ -13,14 +13,27 @@ from adafruit_led_animation.group import AnimationGroup
 from adafruit_led_animation.sequence import AnimationSequence
 from adafruit_led_animation.color import *
 
-pixels = neopixel.NeoPixel(board.D10, 144 + 7, brightness=0.1, auto_write=True)
+pixels = neopixel.NeoPixel(board.D10, 144 + 49, brightness=1, auto_write=True)
 
-led_strip = PixelSubset(pixels, 8, 151)
-star = PixelSubset(pixels, 0, 7)
+star_1 = PixelSubset(pixels, 0, 6)
+star_2 = PixelSubset(pixels, 7, 13)
+star_3 = PixelSubset(pixels, 14, 20)
+star_4 = PixelSubset(pixels, 21, 27)
+star_5 = PixelSubset(pixels, 28, 34)
+star_6 = PixelSubset(pixels, 35, 41)
+star_7 = PixelSubset(pixels, 42, 48)
+led_strip = PixelSubset(pixels, 49, 151)
+
 
 animations = AnimationSequence(
     AnimationGroup(
-        Pulse(star, speed=0.05, color=PURPLE, period=2),
+        Pulse(star_1, speed=0.05, color=PURPLE, period=2),
+        Pulse(star_2, speed=0.05, color=PURPLE, period=2),
+        Pulse(star_3, speed=0.05, color=PURPLE, period=2),
+        Pulse(star_4, speed=0.05, color=PURPLE, period=2),
+        Pulse(star_5, speed=0.05, color=PURPLE, period=2),
+        Pulse(star_6, speed=0.05, color=PURPLE, period=2),
+        Pulse(star_7, speed=0.05, color=PURPLE, period=2),
         Comet(led_strip, speed=0.01, color=AMBER, tail_length=20),
         sync=True,
     )
